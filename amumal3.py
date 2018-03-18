3751dfba3bb602e412bdd0a487a025a5ed89e162
ItsLunaNyan <17682399+LunaNyan@users.noreply.github.com>
2018-03-18 14:40:25 +0900

    Add files via upload

--- amumal3.py
+++ amumal3.py
@@ -2,8 +2,11 @@
 
 from datetime import datetime
 
-print ("아무말3 for Python Alpha002")
+repourl = "https://github.com/LunaNyan/AmuMal3"
+
+print ("아무말3 for Python Alpha003")
 print ("by 루냥이, at 20180318, Twitter @ItsLunaNyan")
+print ("Repo " + repourl)
 print (" ")
 
 help01 = "이 프로그램은 타임라인 방식으로 메모를 기록하는 프로그램입니다."
@@ -37,7 +40,8 @@ while 1:
         print (" ")
         print ("exit        프로그램이 종료됩니다.")
         print (" ")
-        print ("자세한 정보는 알 게 뭐야")
+        print ("자세한 정보는 해당 프로그램에 대한 GitHub 레포지토리")
+        print ("(" + repourl + ")를 참조해주세요.")
     else:
         logf = open("amumal.txt", "a")
         logf.write(inp + "\n" + str(datetime.now()) + "\n----------\n")
