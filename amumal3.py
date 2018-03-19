c4850a46f688c8425a1fb935e06bec721ae0651d
ItsLunaNyan <17682399+LunaNyan@users.noreply.github.com>
2018-03-19 14:07:38 +0900

    add cls command

--- amumal3.py
+++ amumal3.py
@@ -2,16 +2,20 @@
 
 from datetime import datetime
 
+#Global
+versiontext = "Alpha004"
+
+#중복되는 문자열 정리
 repourl = "https://github.com/LunaNyan/AmuMal3"
-
-print ("아무말3 for Python Alpha003")
-print ("by 루냥이, at 20180318, Twitter @ItsLunaNyan")
-print ("Repo " + repourl)
-print (" ")
-
 help01 = "이 프로그램은 타임라인 방식으로 메모를 기록하는 프로그램입니다."
 help02 = "간단히 메모할 내용을 입력하고 Enter을 눌러 기록합니다."
 help03 = "메모할 내용 대신 원하는 명령어를 입력하면 동작이 수행됩니다."
+
+#Startup
+print ("아무말3 for Python " + versiontext)
+print ("by 루냥이, at 20180318, Twitter @ItsLunaNyan")
+print ("Repo " + repourl)
+print (" ")
 
 logfoo = open("amumal.txt", "w")
 logfoo.close()
@@ -26,6 +30,7 @@ if logr.read(8) == "":
     print ("명령어에 대한 자세한 내용이 필요하면 help를 입력하세요.")
     print (" ")
 
+#Loop
 while 1:
     inp = input("입력 : ")
     if inp == "exit":
@@ -42,9 +47,12 @@ while 1:
         print ("명령어와 동작은 다음과 같습니다.")
         print (" ")
         print ("exit        프로그램이 종료됩니다.")
+        print ("cls         화면이 청소됩니다.")
         print (" ")
         print ("자세한 정보는 해당 프로그램에 대한 GitHub 레포지토리")
         print ("(" + repourl + ")를 참조해주세요.")
+    elif inp == "cls":
+        print ("\n" * 100)
     else:
         logf = open("amumal.txt", "a")
         logf.write(inp + "\n" + str(datetime.now()) + "\n----------\n")
